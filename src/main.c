0a8e4ee7151a0eaf0c08a7caa09e3e35d489a40f
Makiesse Morais <makiesse.dev@gmail.com>
2024-11-24 09:29:29 +0100

    refactor: dinamic alloc

--- src/main.c
+++ src/main.c
@@ -34,13 +34,15 @@ int	check_valid_args(char **argv)
 int	main(int argc, char **argv)
 {
 	t_main		main;
-	t_philo			philos[PHILO_MAX];
-	pthread_mutex_t	forks[PHILO_MAX];
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
 
 	if (argc != 5 && argc != 6)
 		return (write(2, "Wrong argument count\n", 22), 1);
 	if (check_valid_args(argv) == 1)
 		return (1);
+	philos = malloc(sizeof(t_philo) * ft_atoi(argv[1]));
+	forks = malloc(sizeof(t_philo) * ft_atoi(argv[1]));
 	init_main(&main, philos);
 	init_forks(forks, ft_atoi(argv[1]));
 	init_philos(philos, &main, forks, argv);
